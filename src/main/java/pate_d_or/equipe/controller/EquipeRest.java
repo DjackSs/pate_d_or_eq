8bf6d5d1d4b079bbbb666489581bc8fff8b22801
unknown <nathan.hamon@3wa.io>
2024-02-21 10:41:18 +0100

    maj structure du projet

---
+++ src/main/java/pate_d_or/equipe/controller/EquipeRest.java
@@ -0,0 +1,12 @@
+package pate_d_or.equipe.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/pate_d_or")
+public class EquipeRest {
+
+}
