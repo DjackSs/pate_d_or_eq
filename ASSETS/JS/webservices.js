4803446badc635748a176492e2f2cf9bf66712e7
Julien Bisson <julien.bisson2@gmail.com>
2024-02-21 15:57:58 +0100

    add assets folder with html, css, js

---
+++ ASSETS/JS/webservices.js
@@ -0,0 +1,38 @@
+const BASE_URL = "http://localhost:8080";
+let container = document.getElementById("container");
+
+loadPage();
+
+let logout = document.getElementById("logout");
+logout.addEventListener("click", logUserOut);
+
+function loadPage() {
+  if (!sessionStorage.getItem("token")) {
+    location = "login.html";
+  }
+  fetch(BASE_URL + "/protected", {
+    method: "GET", 
+    headers: {
+      "token": sessionStorage.getItem("token")
+    }
+  })
+  .then(response => response.text())
+  .then(content => displayContent(content));
+}
+
+function displayContent(content) {
+  container.textContent = content;
+}
+
+function logUserOut() {
+  fetch(BASE_URL + "/logout", {
+    method: "GET"
+    headers: {
+      "token": sessionStorage.getItem("token")
+    }
+  })
+  .then(_ => {
+    sessionStorage.clear();
+    location = "login.html";
+  });
+}
