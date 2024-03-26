2ee2e72da08a3dced65f00c906e25def9c11b2bb
Pierreoliviermpo <po.mpolesha@gmail.com>
2024-03-26 18:37:44 +0100

    new resa endpoint by id table

--- src/main/java/pate_d_or/equipe/dal/ReservationDAO.java
+++ src/main/java/pate_d_or/equipe/dal/ReservationDAO.java
@@ -1,10 +1,14 @@
 package pate_d_or.equipe.dal;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import pate_d_or.equipe.entities.Reservation;
 
 public interface ReservationDAO extends CrudRepository<Reservation, Integer> 
 {
+
+	List<Reservation> findByTableId(int id);
 
 }
