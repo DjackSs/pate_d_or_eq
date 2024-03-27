0a910aba930979331722d645354c36177516208e
unknown <julien.bisson2@gmail.com>
2024-03-27 15:56:10 +0100

    add method for display detail order by table

--- src/main/java/pate_d_or/equipe/bll/RestaurantOrderBLL.java
+++ src/main/java/pate_d_or/equipe/bll/RestaurantOrderBLL.java
@@ -1,11 +1,14 @@
 package pate_d_or.equipe.bll;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pate_d_or.equipe.dal.RestaurantOrderDAO;
+import pate_d_or.equipe.dto.BillDTO;
 import pate_d_or.equipe.entities.Dish;
 import pate_d_or.equipe.entities.RestaurantOrder;
 
@@ -29,6 +32,19 @@ public class RestaurantOrderBLL {
 		}
 		
 		return restaurantOrderDao.findById(id).get();
+	}
+
+	public List<BillDTO> getDetailBillWhereStateSoldAndOrderByIdTable() {
+		List<Object[]> result = this.restaurantOrderDao.getDetailBillWhereStateSoldAndOrderByIdTable();
+		List<BillDTO> bills = new ArrayList<>();
+		for (Object[] current : result) {
+			BillDTO bill = new BillDTO();
+			bill.setTableNumber((int) current[0]);
+			bill.setDishName((String) current[1]);
+			bill.setDishPrice((BigDecimal) current[2]);
+			bills.add(bill);
+		}
+		return bills;
 	}
 
 	public Float getTotalAmountOrderBillById(int id)
